c1b44aa4a75bf4851e29cd3c4754417583a18228
“dimitri-feniou” <dimitri.feniou@gmail.com>
2024-12-08 18:00:24 +0100

    remove folder

--- openclassrooms-projet7-analyse-sentiment-master/api_sentiment_analysis/tests/confest.py
+++
@@ -1,8 +0,0 @@
-import os
-import pytest
-
-
-@pytest.fixture(scope="session", autouse=True)
-def set_working_directory():
-    # Définit le dossier de travail comme la racine du projet
-    os.chdir(os.path.abspath(os.path.dirname(__file__)) + "/..")
